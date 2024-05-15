cff8f07e84943f9e690c1b29ed26dc1a6f13b847
DimitriosIlio <dimitriosilio@gmail.com>
2024-05-15 19:28:50 +0200

    newnew

--- Desktop/sta mpeta/staycation/staycation-frontend/src/pages/BookARoom/Popup.js
+++ Desktop/sta mpeta/staycation/staycation-frontend/src/pages/BookARoom/Popup.js
@@ -1,14 +1,24 @@
-/* Popup.js */
-
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Popup = ({ onClose }) => {
   const [paymentProcessed, setPaymentProcessed] = useState(false);
+  const navigate = useNavigate();
 
   const handlePayment = () => {
     setPaymentProcessed(true);
     // Here you can add any other logic for processing the payment
   };
+
+  useEffect(() => {
+    if (paymentProcessed) {
+      const timer = setTimeout(() => {
+        navigate('/');
+      }, 5000);
+
+      return () => clearTimeout(timer); // Cleanup the timer on unmount
+    }
+  }, [paymentProcessed, navigate]);
 
   return (
     <div className="popup">
@@ -20,7 +30,7 @@ const Popup = ({ onClose }) => {
             <button onClick={onClose}>Close</button>
           </>
         ) : (
-          <h2>Booked!!!</h2>
+          <h2>Booked!!! Check your emails for confirmation.</h2>
         )}
       </div>
     </div>
